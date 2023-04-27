8e2bea6eb12d19f5f3aa6c555669f2935094b6b7
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2023-04-27 13:05:43 +0100

    store prop

---
+++ src/state/reducers/index.js
@@ -0,0 +1,9 @@
+// combine reducers 
+import {combineReducers} from "redux"
+import accountReducer from "./accountReducer"
+
+const reducers = combineReducers({
+    account: accountReducer
+})
+
+export default reducers

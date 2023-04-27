621b7454bcca1a5ad87ee5f00faac1e8ee830b26
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2023-04-27 13:18:17 +0100

    actions

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import { useSelector } from 'react-redux';
 
 function App() {
+// state inside the store we want to return the state 
+  const account = useSelector((state) =>state)
+  // we can use it any part of the app
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ 
     </div>
   );
 }
---
+++ src/state/action-creators/index.js
@@ -0,0 +1,5 @@
+// this wiill hoiuse all our actions
+
+export  const depositMoney = (amount) => {
+
+}
